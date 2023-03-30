8405f6083aef0be0acbb152ad4d45f1e08690797
Simplify-ERP® <github@euroblaze.de>
2023-03-30 18:08:14 +0200

    Update lexoffice_contact.py

--- models/lexoffice_contact.py
+++ models/lexoffice_contact.py
@@ -1,3 +1,56 @@
+from odoo import api, fields, models
+import requests
+
+
+class LexofficeContact(models.Model):
+    _name = 'lexoffice.contact'
+    _description = 'Lexoffice Contact'
+
+    name = fields.Char('Name')
+    street = fields.Char('Street')
+    zip = fields.Char('ZIP')
+    city = fields.Char('City')
+    country_id = fields.Many2one('res.country', 'Country')
+    phone = fields.Char('Phone')
+    email = fields.Char('Email')
+
+    @api.model
+    def sync_lexoffice_contacts(self, api_key):
+        base_url = 'https://api.lexoffice.io/v1/'
+        headers = {'Authorization': f'Bearer {api_key}', 'Accept': 'application/json'}
+        url = f'{base_url}contacts'
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            contacts = response.json()['content']
+            for contact in contacts:
+                self._create_update_contact(contact)
+        else:
+            raise ValueError(f'Error fetching contacts from Lexoffice API: {response.text}')
+
+    def _create_update_contact(self, contact_data):
+        name = contact_data['company']['name']
+        address = contact_data['addresses'][0]
+        country_code = address['countryCode']
+        country = self.env['res.country'].search([('code', '=', country_code)], limit=1)
+
+        vals = {
+            'name': name,
+            'street': address['street'],
+            'zip': address['zip'],
+            'city': address['city'],
+            'country_id': country.id,
+            'phone': address['phone'],
+            'email': address['email'],
+        }
+
+        existing_contact = self.search([('name', '=', name)], limit=1)
+        if existing_contact:
+            existing_contact.write(vals)
+        else:
+            self.create(vals)
+
+
 @api.model
 def sync_lexoffice_contacts(self, api_key, existing_lexoffice_ids=None):
     # ...
