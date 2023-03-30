e6020a01cd36192d574d51ce3924637c8104bdf0
Simplify-ERP® <github@euroblaze.de>
2023-03-30 18:03:17 +0200

    Update lexoffice_contact.py

--- models/lexoffice_contact.py
+++ models/lexoffice_contact.py
@@ -1,5 +1,13 @@
-
-
+@api.model
+def sync_lexoffice_contacts(self, api_key, existing_lexoffice_ids=None):
+    # ...
+    if response.status_code == 200:
+        contacts = response.json()['content']
+        for contact in contacts:
+            if existing_lexoffice_ids and contact['id'] in existing_lexoffice_ids:
+                continue
+            self._create_update_contact(contact)
+    # ...
 
 @api.model
 def sync_new_lexoffice_data(self, api_key):
@@ -7,3 +15,10 @@ def sync_new_lexoffice_data(self, api_key):
     self.env['lexoffice.product'].sync_new_lexoffice_products(api_key)
     self.env['lexoffice.order'].sync_new_lexoffice_orders(api_key)
     self.env['lexoffice.invoice'].sync_new_lexoffice_invoices(api_key)
+
+@api.model
+def sync_new_lexoffice_contacts(self, api_key):
+    contacts = self.env['res.partner'].search([('lexoffice_id', '!=', False)])
+    existing_lexoffice_ids = [contact.lexoffice_id for contact in contacts]
+    self.sync_lexoffice_contacts(api_key, existing_lexoffice_ids)
+
